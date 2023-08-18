5644568e67023870bb011a44250555529c55a724
Saurabh varule <varulesaurabh44@gmail.com>
2023-08-18 19:02:56 +0530

    Relational Operator

---
+++ Dart/1808/Program5.dart
@@ -0,0 +1,14 @@
+// Relational Operaotor
+// <  >  <=  >=  ==  !=
+
+void main() {
+  int x = 10;
+  int y = 5;
+
+  print(x < y); // false
+  print(x > y); // true
+  print(x <= y); // false
+  print(x >= y); // true
+  print(x == y); // false
+  print(x != y); // true
+}
