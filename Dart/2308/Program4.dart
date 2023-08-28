963b9a4c14e822ca40a7c5a4cf5b0c56190ca6fe
Saurabh varule <varulesaurabh44@gmail.com>
2023-08-28 12:57:49 +0530

    do while loop

---
+++ Dart/2308/Program4.dart
@@ -0,0 +1,11 @@
+// do while loop (exit control loop)
+
+void main() {
+  int i = 0;
+  do {
+    print("the value of i is 0");
+  } while (i != 0);
+}
+
+// do-while loop checks the condition at the end of loop.
+// a do-while loop is iterates loop at least one time if condition is false/
