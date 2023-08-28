3bce6e98fd90c59b6458d516c138a0651c322a4a
Saurabh varule <varulesaurabh44@gmail.com>
2023-08-28 12:52:35 +0530

    while loop

---
+++ Dart/2308/Program3.dart
@@ -0,0 +1,19 @@
+// while loop (entry control)
+
+void main() {
+  int i = 1;
+  while (i <= 10) {
+    print("The value of i is $i");
+    i++;
+  }
+}
+/*
+while loop prototype :
+
+int i = 1;    -->> initialization
+while(i<=10)  -->> condition
+{
+  ////Body///
+  i++;        -->> increment / decrement
+}
+*/
