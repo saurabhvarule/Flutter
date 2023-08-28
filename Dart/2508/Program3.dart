e9a320cdcb616f4cc343c6e001a9fd4dda8d12cd
Saurabh varule <varulesaurabh44@gmail.com>
2023-08-28 13:20:16 +0530

    Switch Case

---
+++ Dart/2508/Program3.dart
@@ -0,0 +1,15 @@
+// switch
+
+void main() {
+  int x = 3;
+  switch (x) {
+    case 1:
+      print("x = $x");
+    case 2:
+      print("x = $x");
+    case 3:
+      print("x = $x");
+    default:
+      print("No match");
+  }
+}
