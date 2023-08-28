fbc3740bfa3a61f65e3c55aeb3660524862c722f
Saurabh varule <varulesaurabh44@gmail.com>
2023-08-28 12:48:04 +0530

    if else

---
+++ Dart/2308/Program1.dart
@@ -0,0 +1,16 @@
+// control statements
+// 1. if
+// 2. else
+// 3. else if
+
+void main() {
+  int x = 10;
+  int y = 5;
+  if (x < y) {
+    print("x is less than y");
+  } else if (x > y) {
+    print("x is greater thean y");
+  } else {
+    print("Both x and y are equal");
+  }
+}
