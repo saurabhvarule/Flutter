fb4d291f53de2c8e482d8dcc03cf26338ce3ea23
Saurabh varule <varulesaurabh44@gmail.com>
2023-10-02 17:14:32 +0530

    ways to write different parameterized constructor

--- Dart/OOP/Constructor/Program2.dart
+++ Dart/OOP/Constructor/Program2.dart
@@ -1,4 +1,7 @@
 class Demo {
+
+  int? x = 10;
+  int? y = 20;
 // 1.
 /*  Demo() {
     print("No argument Constructor");
@@ -14,13 +17,26 @@ class Demo {
   Demo(int x, [int y = 20]) {
     print("optional parameter constructor");
   }
-*/
+
 // 4.
   Demo(int x, {int y = 20}) {
     print("Default parameter constructor");
   }
+
+*/
+
+// 5.
+  Demo({this.y, this.x}) {
+    print("Named parameter");
+  }
+
+  void info() {
+    print(x);
+    print(y);
+  }
 }
 
 void main() {
-  Demo obj = new Demo(10);
+  Demo obj = new Demo(y: 20, x: 10);
+  obj.info();
 }
