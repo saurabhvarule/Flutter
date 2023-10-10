a180fe4bcb9f719deea59e354f44501232975424
Saurabh varule <varulesaurabh44@gmail.com>
2023-10-10 17:52:51 +0530

    Operators

---
+++ Dart/Operators/Program4.dart
@@ -0,0 +1,13 @@
+// 2.Arithmatic Operators
+// +  -  *  /  %  ~/
+
+void main() {
+  int x = 12;
+  int y = 8;
+  print(x + y); // 20
+  print(x - y); // 4
+  print(x / y); // 1.5
+  print(x * y); // 96
+  print(x % y); // 4
+  print(x ~/ y); // 1
+}
